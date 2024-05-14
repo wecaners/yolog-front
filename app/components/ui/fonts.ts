82acf0385e136594c49003d0ff5fd3ea6ba06553
wecaners <wecaners@naver.com>
2024-05-14 12:01:25 +0900

    chore: 프로젝트 초기세팅

---
+++ app/components/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Gowun_Batang } from 'next/font/google';
+
+export const gowunBatang = Gowun_Batang({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,51 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+
+type AccType = Record<string, string>;
+
+const range = (start: number, end: number): number[] => {
+  let array = [];
+  for (let i = start; i <= end; ++i) {
+    array.push(i);
+  }
+  return array;
+};
+
+const pxToRem = (px: number, base = 16) => `${px / base}rem`;
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    spacing: {
+      ...range(0, 2000).reduce((acc: AccType, px: number) => {
+        acc[`${px}pxr`] = pxToRem(px);
+        return acc;
+      }, {}),
+    },
+    fontSize: {
+      ...range(0, 2000).reduce((acc: AccType, px: number) => {
+        acc[`${px}pxr`] = pxToRem(px);
+        return acc;
+      }, {}),
+    },
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontFamily: {
+        pretendard: ['var(--font-pretendard)'],
+      },
+      colors: {
+        primary100: '#EEF9E7',
+        primary200: '#DDEEC5',
+        primary300: '#AFC98D',
+        primary400: '#8BA47B',
+        primary500: '#506046',
       },
     },
   },
