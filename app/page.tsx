06e94df9973a639ec2e44a322e3a25729f9b23f3
wecaners <wecaners@naver.com>
2024-05-18 01:16:31 +0900

    feat: input 컴포넌트 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+import Input from './components/input';
+import { PROFILE_NAME_VALIDATION } from './lib/constants/validation';
+
 export default function Home() {
-  return <h1>YOLOG</h1>;
+  return (
+    <div className="flex justify-center items-center w-500pxr h-500pxr bg-white">
+      <Input
+        placeholder="프로필 이름을 입력해주세요."
+        registerName="profileName"
+        title="프로필 이름"
+        type="text"
+        rules={PROFILE_NAME_VALIDATION}
+        validMessage="사용할 수 있는 이름이에요."
+      />
+    </div>
+  );
 }
