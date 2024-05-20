e04b63f41a488b342148e4ed3b7e3020694e2c9c
wecaners <wecaners@naver.com>
2024-05-21 00:44:46 +0900

    refactor: dialog 컴포넌트 usePortal 적용

--- app/components/dialog.tsx
+++ app/components/dialog.tsx
@@ -1,5 +1,6 @@
 'use client';
-import React, { ReactNode, useEffect, useState } from 'react';
+import usePortal from '@/hooks/use-portal';
+import React, { ReactNode } from 'react';
 import { createPortal } from 'react-dom';
 
 interface IDialogProps {
@@ -17,26 +18,24 @@ function Dialog({
   dialogRef,
   dialogOutsideClick,
 }: IDialogProps) {
-  const [mounted, setMounted] = useState(false);
+  const portalRoot = usePortal('dialog-portal');
 
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!isOpen || !mounted) return null;
-  return createPortal(
-    <div>
-      <div
-        ref={dialogRef}
-        onClick={dialogOutsideClick}
-        className="z-4 fixed bottom-0pxr left-0pxr right-0pxr top-0pxr bg-black bg-opacity-50"
-      />
-      <div className="z-4 fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-276pxr py-16pxr px-9pxr gap-20pxr rounded-[20px] shadow-dialog bg-white text-black">
-        {children}
-      </div>
-    </div>,
-    document.getElementById('global-dialog') as HTMLElement,
-  );
+  if (!isOpen) return null;
+  return portalRoot
+    ? createPortal(
+        <div>
+          <div
+            ref={dialogRef}
+            onClick={dialogOutsideClick}
+            className="z-4 fixed bottom-0pxr left-0pxr right-0pxr top-0pxr bg-black bg-opacity-50"
+          />
+          <div className="z-4 fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-276pxr py-16pxr px-9pxr gap-20pxr rounded-[20px] shadow-dialog bg-white text-black">
+            {children}
+          </div>
+        </div>,
+        portalRoot,
+      )
+    : null;
 }
 
 export default Dialog;
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 'use client';
+import UseDialog from '@/hooks/use-dialog';
 import useToast from '@/hooks/use-toast';
+import { Dialog, DialogLogout } from './components';
+import { useRef } from 'react';
 
 export default function Home() {
   const showToast = useToast();
+  const { isDialogOpen, dialogOutsideClick, setIsDialogOpen, dialogRef } =
+    UseDialog();
 
   return (
-    <div>
+    <div className="flex gap-20pxr">
       <button
-        className="w-200pxr h-120pxr bg-primary300"
+        className="w-200pxr h-120pxr bg-primary300 rounded-2xl"
         onClick={() =>
           showToast({
             message: '성공적으로 토스트가 생성됐습니다!',
@@ -17,6 +22,20 @@ export default function Home() {
       >
         토스트 테스트 버튼
       </button>
+      <button
+        className="w-200pxr h-120pxr bg-primary300 rounded-2xl"
+        onClick={() => setIsDialogOpen(!isDialogOpen)}
+      >
+        모달 테스트 버튼
+      </button>
+
+      <Dialog
+        isOpen={isDialogOpen}
+        dialogOutsideClick={dialogOutsideClick}
+        dialogRef={dialogRef}
+      >
+        <DialogLogout />
+      </Dialog>
     </div>
   );
 }
