e29dff4ad2cf6574517ecfde977adf217402742b
wecaners <wecaners@naver.com>
2024-05-21 00:30:43 +0900

    refactor: 불필요한 내용 삭제

--- hooks/use-toast.ts
+++ hooks/use-toast.ts
@@ -1,5 +1,5 @@
 import { addToast, removeToast } from '@/app/lib/redux/toast-slice';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 interface IUseToastProps {
   message: string;
