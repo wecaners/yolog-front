4e0428ec642632ba8f44a5f1df93c4cfff23bc8c
정성휘 <whistle_js@naver.com>
2024-05-22 17:44:14 +0900

    feat: button 컴포넌트 구현

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,5 +1,6 @@
 export { default as Checkbox } from './checkbox';
 export { default as Input } from './input';
+export { default as Button } from './ui/button';
 export { default as Dialog } from '../components/dialog';
 export { default as DialogLogout } from '../components/ui/dialog-logout';
 export { default as ToastContainer } from './toast-container';
---
+++ app/components/ui/button.tsx
@@ -0,0 +1,42 @@
+interface Props {
+  disabled?: boolean;
+  angled?: boolean;
+  size?: 'large' | 'small';
+  styles?: string;
+  children?: string | JSX.Element | (string | JSX.Element)[];
+  onClick?: () => void;
+}
+
+function Button({
+  disabled,
+  angled,
+  size = 'large',
+  styles = 'bg-primary400 text-white',
+  children,
+  onClick,
+}: Props) {
+  const sizeStyles = {
+    large: `w-full h-58pxr text-18pxr font-bold`,
+    small: 'px-18pxr py-10pxr text-16pxr font-semibold',
+  };
+
+  const getButtonActiveStyle = () => {
+    if (disabled) {
+      return 'bg-[#e3e3e3] text-[#9a9a9a] font-semibold';
+    }
+
+    return styles;
+  };
+
+  return (
+    <button
+      className={`${sizeStyles[size]} ${getButtonActiveStyle()} ${!angled && 'rounded-[15px]'}`}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
