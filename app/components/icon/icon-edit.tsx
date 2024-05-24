78061bee591fdd4ea048fe8d7a2f712c68bd00d6
정성휘 <whistle_js@naver.com>
2024-05-25 00:42:32 +0900

    feat: 아이콘 컴포넌트 추가

---
+++ app/components/icon/icon-edit.tsx
@@ -0,0 +1,30 @@
+function IconEdit({ size = 28 }: { size?: number }) {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 28 28"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        id="Vector"
+        d="M12.704 3.75586H3.6009C2.9111 3.75586 2.24955 4.02988 1.76178 4.51764C1.27402 5.00541 1 5.66696 1 6.35676V24.563C1 25.2528 1.27402 25.9144 1.76178 26.4021C2.24955 26.8899 2.9111 27.1639 3.6009 27.1639H21.8072C22.497 27.1639 23.1585 26.8899 23.6463 26.4021C24.1341 25.9144 24.4081 25.2528 24.4081 24.563V15.4599"
+        stroke="white"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        id="Vector_2"
+        d="M22.4574 1.808C22.9748 1.29064 23.6765 1 24.4081 1C25.1398 1 25.8414 1.29064 26.3588 1.808C26.8761 2.32535 27.1668 3.02702 27.1668 3.75867C27.1668 4.49031 26.8761 5.19199 26.3588 5.70934L14.0045 18.0636L8.80273 19.364L10.1032 14.1623L22.4574 1.808Z"
+        stroke="white"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default IconEdit;
--- app/components/index.ts
+++ app/components/index.ts
@@ -6,3 +6,5 @@ export { default as DialogLogout } from '../components/ui/dialog-logout';
 export { default as FloatingButton } from './ui/floating-button';
 export { default as SocialButton } from './ui/social-button';
 export { default as ToastContainer } from './toast-container';
+
+export * from './icon';
