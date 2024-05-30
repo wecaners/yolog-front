3665c368647158f993029c40cc077c2ca8b00406
wecaners <wecaners@naver.com>
2024-05-30 19:22:51 +0900

    refactor: input storybook 에러 해결

--- stories/default-input.stories.tsx
+++ stories/default-input.stories.tsx
@@ -1,8 +1,9 @@
 import { Input } from '@/app/components';
 import { PROFILE_NAME_VALIDATION } from '@/app/lib/constants/validation';
 import type { Meta, StoryObj } from '@storybook/react';
+import { useForm } from 'react-hook-form';
 
-const meta = {
+const meta: Meta<typeof Input> = {
   title: 'Input/default-input',
   component: Input,
   parameters: {
@@ -24,9 +25,10 @@ const meta = {
       description: '프로필 이름을 입력해주세요.',
     },
   },
-} satisfies Meta<typeof Input>;
+};
 
 export default meta;
+
 type Story = StoryObj<typeof meta>;
 
 export const ProfileInput: Story = {
@@ -37,5 +39,24 @@ export const ProfileInput: Story = {
     placeholder: '프로필 이름을 입력해주세요.',
     registerName: 'profileName',
     rules: PROFILE_NAME_VALIDATION,
+    defaultMessage: `4~12자 이내로 입력해주세요.
+이름은 14일에 한 번 변경할 수 있어요.`,
+  },
+  render: (args) => {
+    const {
+      register,
+      watch,
+      formState: { errors },
+    } = useForm();
+    const profileName = watch(args.registerName, '');
+
+    return (
+      <Input
+        {...args}
+        register={register}
+        errors={errors}
+        value={profileName}
+      />
+    );
   },
 };
