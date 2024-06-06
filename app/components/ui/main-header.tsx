2368fa0c1b636862aa58d0e2f5270f767d6bcad1
wecaners <wecaners@naver.com>
2024-06-06 16:08:47 +0900

    refactor: 메인 헤더 디자인 변경

--- app/components/ui/main-header.tsx
+++ app/components/ui/main-header.tsx
@@ -10,7 +10,6 @@ import useMenu from '@/hooks/use-menu';
 function MainHeader() {
   const { isMenuOpen, setIsMenuOpen } = useMenu();
 
-  const [tabs, setTabs] = useState('일기');
   const selectedTabStyle =
     'text-black font-bold border-b-4 border-[#506046] rounded-[2px] opacity-100';
   const handleClose = () => {
@@ -21,22 +20,16 @@ function MainHeader() {
       <Menu isOpen={isMenuOpen}>
         <MainMenu onClose={handleClose} />
       </Menu>
-      <div className="mx-16pxr flex justify-between pt-20pxr items-center">
-        <div className="flex gap-28pxr">
-          <button
-            className={`${gowunBatang.className} text-17pxr font-bold opacity-[0.25] pb-4pxr ${tabs === '일기' ? selectedTabStyle : ''}`}
-            onClick={() => setTabs('일기')}
-          >
-            여행 일기
-          </button>
-          <button
-            className={`${gowunBatang.className} text-17pxr font-bold opacity-[0.25] pb-4pxr ${tabs === '메모' ? selectedTabStyle : ''}`}
-            onClick={() => setTabs('메모')}
-          >
-            빠른 메모
-          </button>
-        </div>
-        <button className="pb-4pxr" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+      <div className="mx-16pxr flex justify-center pt-20pxr items-center">
+        <span
+          className={`text-primary400 ${gowunBatang.className} text-20pxr font-bold`}
+        >
+          여록하다
+        </span>
+        <button
+          className="absolute right-16pxr"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
           <IconMenu />
         </button>
       </div>
